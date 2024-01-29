8eff63eb6bc56cf867a64980ed77e9132840c864
HamzaHajMtir1 <hamzahajmtir2002@gmail.com>
2024-01-29 22:53:25 +0100

    Adding Caracters Fonctionality

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-first',
   templateUrl: './first.component.html',
   styleUrl: './first.component.css'
 })
-export class FirstComponent {
+export class FirstComponent implements OnInit{
+
+  marvel ={
+    name:"",
+    power:"",
+    imageURL:""
+  }
+
+  hero :any[] = [];
+
+  constructor() {
+
+  }
+  ngOnInit(): void {
+
+  }
+
+  add(){
+    this.hero.push(this.marvel);
+    this.marvel = {
+      name:"",
+      power:"",
+      imageURL:""
+    }
+
+    console.log(this.hero)
+  }
 
 }
